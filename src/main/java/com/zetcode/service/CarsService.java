9b3b6ca8bc0dfb35e0e887724e68e87790c018fb
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-16 14:22:54 +0100

    Create CarsService.java

---
+++ src/main/java/com/zetcode/service/CarsService.java
@@ -0,0 +1,30 @@
+package com.zetcode.service;
+
+import com.zetcode.bean.Car;
+import com.zetcode.persistence.CarDAO;
+import com.zetcode.persistence.JdbcDAO;
+import java.util.List;
+
+public class CarsService implements ICarsService {
+
+    @Override
+    public List<Car> findAllCars() {
+
+        CarDAO carDAO = new JdbcDAO();
+        return carDAO.findAll();
+    }
+
+    @Override
+    public Car findCar(Long id) {
+
+        CarDAO carDAO = new JdbcDAO();
+        return carDAO.findCar(id);
+    }
+
+    @Override
+    public void saveCar(Car car) {
+        
+        CarDAO carDAO = new JdbcDAO();
+        carDAO.saveCar(car);
+    }
+}
