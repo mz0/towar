121e4843158c48998479e00cd8b29e9e6183b851
Mark Zhitomirski <marcuzero@gmail.com>
2020-05-20 13:05:30 +0300

    add mock 'cores' (and drop jooq)

---
+++ multicore/src/main/java/org/example/CoreCustom.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoreCustom {
+    private static Logger log = LoggerFactory.getLogger("Example");
+    public CoreCustom() {
+        log.info("Custom Core initialized");
+    }
+}
---
+++ multicore/src/main/java/org/example/CoreGeneric.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class CoreGeneric {
+    public CoreGeneric() {
+        System.out.println("Generic core initialized");
+    }
+}
